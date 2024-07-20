e736b454221365f881b359b1b032b56cd4f1af4a
Francis <francisnjaramba2@gmail.com>
2024-07-20 16:33:57 +0300

    Registration form complete

--- big/big/settings.py
+++ big/big/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'small'
+    'small',
 ]
 
 
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'public/static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- big/small/urls.py
+++ big/small/urls.py
@@ -1,7 +1,24 @@
+# Import necessary modules
+from django.contrib import admin # Django admin module
 from django.urls import path
+from django.conf.urls.static import static
+
+# from big.big.settings import STATIC_URL	 # URL routing
 from . import views
+from . views import * # Import views from the authentication app
+from django.conf import settings # Application settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # Static files serving
 
+# Define URL patterns
 urlpatterns = [
-    path('', views.home, name='home'),
-
+    path('', views.home, name='home'),  # URL for the home page
+    path('login/', views.login_page, name='login'),  # URL for the login page
+    path('register/', views.register_page, name='register'),  # URL for the registration page
 ]
+
+# Serve media files if DEBUG is True (development mode)
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files using staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
